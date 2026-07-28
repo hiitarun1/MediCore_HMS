10eb6c45e80d6545299f3d284276ff30ced2e034
hiitarun1 <ktarunkushwaha@gmail.com>
2026-07-28 11:14:57 +0530

    Refactor controllers to use DTOs

--- src/main/java/com/tarun/HospitalManagement/controller/DoctorController.java
+++ src/main/java/com/tarun/HospitalManagement/controller/DoctorController.java
@@ -1,12 +1,15 @@
 package com.tarun.HospitalManagement.controller;
 
-import com.tarun.HospitalManagement.entity.Appointment;
-import com.tarun.HospitalManagement.entity.Department;
-import com.tarun.HospitalManagement.entity.Doctor;
+import com.tarun.HospitalManagement.dto.request.DoctorRequestDTO;
+import com.tarun.HospitalManagement.dto.response.AppointmentResponseDTO;
+import com.tarun.HospitalManagement.dto.response.DepartmentResponseDTO;
+import com.tarun.HospitalManagement.dto.response.DoctorResponseDTO;
 import com.tarun.HospitalManagement.service.DoctorService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,19 +26,19 @@ public class DoctorController {
 
     @PostMapping
     @Operation(summary = "Register a new doctor")
-    public ResponseEntity<Doctor> createDoctor(@RequestBody Doctor doctor) {
-        return ResponseEntity.ok(doctorService.createDoctor(doctor));
+    public ResponseEntity<DoctorResponseDTO> createDoctor(@Valid @RequestBody DoctorRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(doctorService.createDoctor(dto));
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Get a doctor by ID")
-    public ResponseEntity<Doctor> getDoctorById(@PathVariable Long id) {
+    public ResponseEntity<DoctorResponseDTO> getDoctorById(@PathVariable Long id) {
         return ResponseEntity.ok(doctorService.getDoctorById(id));
     }
 
     @GetMapping
     @Operation(summary = "Get all registered doctors")
-    public ResponseEntity<List<Doctor>> getAllDoctors() {
+    public ResponseEntity<List<DoctorResponseDTO>> getAllDoctors() {
         return ResponseEntity.ok(doctorService.getAllDoctors());
     }
 
@@ -48,25 +51,25 @@ public class DoctorController {
 
     @PatchMapping("/{id}/email")
     @Operation(summary = "Update doctor's email address")
-    public ResponseEntity<Doctor> updateDoctorEmail(@PathVariable Long id, @RequestParam String email) {
+    public ResponseEntity<DoctorResponseDTO> updateDoctorEmail(@PathVariable Long id, @RequestParam String email) {
         return ResponseEntity.ok(doctorService.updateDoctorEmail(id, email));
     }
 
     @PatchMapping("/{id}/specialization")
     @Operation(summary = "Update doctor's area of specialization")
-    public ResponseEntity<Doctor> updateSpecialization(@PathVariable Long id, @RequestParam String specialization) {
+    public ResponseEntity<DoctorResponseDTO> updateSpecialization(@PathVariable Long id, @RequestParam String specialization) {
         return ResponseEntity.ok(doctorService.updateSpecialization(id, specialization));
     }
 
     @GetMapping("/{id}/appointments")
     @Operation(summary = "Get all appointments assigned to a doctor")
-    public ResponseEntity<List<Appointment>> getDoctorAppointments(@PathVariable Long id) {
+    public ResponseEntity<List<AppointmentResponseDTO>> getDoctorAppointments(@PathVariable Long id) {
         return ResponseEntity.ok(doctorService.getDoctorAppointments(id));
     }
 
     @GetMapping("/{id}/departments")
     @Operation(summary = "Get all departments a doctor belongs to")
-    public ResponseEntity<Set<Department>> getDoctorDepartments(@PathVariable Long id) {
+    public ResponseEntity<Set<DepartmentResponseDTO>> getDoctorDepartments(@PathVariable Long id) {
         return ResponseEntity.ok(doctorService.getDoctorDepartments(id));
     }
 
